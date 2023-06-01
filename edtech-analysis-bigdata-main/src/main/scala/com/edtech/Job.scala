27d63928d8415d03e85a305c942e087987765622
Julik0902 <67378864+Julik0902@users.noreply.github.com>
2023-06-01 22:28:27 +0530

    Delete edtech-analysis-bigdata-main directory

--- edtech-analysis-bigdata-main/src/main/scala/com/edtech/Job.scala
+++
@@ -1,9 +0,0 @@
-package com.edtech
-
-import org.apache.spark.sql.SparkSession
-
-trait Job {
-
-  def execute(spark: SparkSession) : Unit
-
-}
--- edtech-analysis-bigdata-main/src/main/scala/com/edtech/processing/ProcessingConstants.scala
+++
@@ -1,5 +0,0 @@
-package com.edtech.processing
-
-object ProcessingConstants {
-  val SPARK_APPLICATION_NAME = "EDTECH_ANALYSIS_PROCESSING"
-}
